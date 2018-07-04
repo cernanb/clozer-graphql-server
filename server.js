11fa49f75ba1aa1e96a466e9f0a5ca87dbf46e10
Cernan Bernardo <cernanb@gmail.com>
2018-07-04 11:09:41 -0700

    Connect mongoose to mLab

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const schema = require('./schema')
 const app = express()
 
 const PORT = 4000
+
+require('dotenv').config()
+const MONGO_URI = process.env.MONGO_URI
+
+if (!MONGO_URI) {
+  throw new Error('You must provide a MongoLab URI')
+}
+
+mongoose.Promise = global.Promise
+mongoose.connect(MONGO_URI)
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connection to MongoLab: ', error))
 
 app.use(bodyParser.json())
 app.use(
