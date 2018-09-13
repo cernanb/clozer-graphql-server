ca788905dcfe5a35c49854310156f4d2cd34cdb9
github username <github email address>
2018-09-12 20:57:56 -0700

    Update clients query

--- schema/rootQueryType.js
+++ schema/rootQueryType.js
@@ -1,34 +1,39 @@
-const graphql = require('graphql')
-const mongoose = require('mongoose')
+const graphql = require("graphql")
+const mongoose = require("mongoose")
 const { GraphQLObjectType, GraphQLList, GraphQLNonNull, GraphQLID } = graphql
 
-const ClientType = require('./clientType')
-const UserType = require('./userType')
-const Client = mongoose.model('client')
+const ClientType = require("./clientType")
+const UserType = require("./userType")
+const Client = mongoose.model("client")
+const User = mongoose.model("user")
 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
+  name: "RootQueryType",
   fields: () => ({
     clients: {
       type: new GraphQLList(ClientType),
-      resolve() {
-        return Client.find({})
-      },
+      resolve(parentValue, args, req) {
+        return User.findById(req.user.id)
+          .populate("clients")
+          .then(user => {
+            return user.clients
+          })
+      }
     },
     client: {
       type: ClientType,
       args: { id: { type: new GraphQLNonNull(GraphQLID) } },
       resolve(parentValue, { id }) {
         return Client.findById(id)
-      },
+      }
     },
     currentUser: {
       type: UserType,
       resolve(parentValue, args, req) {
         return req.user
-      },
-    },
-  }),
+      }
+    }
+  })
 })
 
 module.exports = RootQuery
